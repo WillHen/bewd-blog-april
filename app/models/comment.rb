c7b59ef0d70365b88e6c76c7a8146c33919966ab
Will H <whenshaw87@googlemail.com>
2015-04-29 15:18:21 +0100

    assigns default value of anon to commenter name or comenter name if present

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
     belongs_to :post
     validates :body, presence: true
     validates :post, presence: true
+    
+    def commenter_name
+       commenter.present? ? self.commenter : "Anon" 
+    end
 end
